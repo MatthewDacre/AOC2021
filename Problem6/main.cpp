d5c7940a2081139e1a403aba761a87a5c5aa6d3f
Matthew Dacre <matthew.dacre1@gmail.com>
2021-12-16 13:49:19 +0200

    Day 6 problem 1

--- Problem6/main.cpp
+++ Problem6/main.cpp
@@ -8,15 +8,69 @@
 
 using namespace std;
 
+long long fish[9];
+
+void printFish(){
+    for(int i = 0; i < 9; i++){
+        cout<<"Timer: "<<i<<" Amount: "<<fish[i]<<endl;
+    }
+}
+
+void printSum(){
+    long long sum = 0;
+    for(int i = 0; i < 9; i++){
+        sum += fish[i];
+    }
+    cout<<sum<<endl;
+}
+
+string counts(){
+    string s;
+    for(int i = 0; i < 9; i++){
+        s += to_string(fish[i]) + ", ";
+    }
+    return s;
+}
+
+void part1(){
+    for(int day = 0; day < 256; day++){
+        //cout<<"Day "<<day<<" : "<<counts()<<endl;
+        //printSum();
+        int nextday[9];
+        for(int i = 1; i < 9; i++){
+            nextday[i-1] = fish[i];
+        }
+        //0 case
+        nextday[6] += fish[0];
+        nextday[8] = fish[0];
+        for(int i = 0; i < 9; i++){
+            fish[i] = nextday[i];
+        }
+        
+    }
+    printFish();
+    printSum();
+}
+
 int main(int argc, char const *argv[])
 {
     fstream file;
-    file.open("input", ios::in);
+    string s;
+    file.open("sample", ios::in);
     if(!file){
         cout<<"Can't open file"<<endl;
         exit(1);
     }
     while(!file.eof()){
+        file>>s;
     }
+    for(char c : s){
+        if(c != ','){
+            int a = c - '0';
+            fish[a]++;
+        }
+    }
+    //printFish();
+    part1();
     return 0;
 }
